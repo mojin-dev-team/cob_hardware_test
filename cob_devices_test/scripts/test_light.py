3f26efde33686197e76fdaaebbd67ce8646e8b59
benmaidel <benjamin.maidel@mojin-robotics.de>
2018-08-27 18:54:09 +0200

    improve test_light

--- cob_devices_test/scripts/test_light.py
+++ cob_devices_test/scripts/test_light.py
@@ -13,29 +13,45 @@ class TestLight():
     def __init__(self):
         self._srv = rospy.ServiceProxy('set_light', SetLightMode)
         self._num_leds = rospy.get_param("num_leds", 1)
+        self._colors = [("red",ColorRGBA(1.0,0.0,0.0,1.0)), ("green",ColorRGBA(0.0,1.0,0.0,1.0)), ("blue", ColorRGBA(0.0,0.0,1.0,1.0))]
+        self._timer = None
+        self._idx = 0
 
-    def execute(self):
+    def start(self):
+        self._timer = rospy.Timer(rospy.Duration(10), self.execute)
+
+    def execute(self, event):
+        rospy.loginfo("Setting color to %s",self._colors[self._idx%3][0])
         req = SetLightModeRequest()
-        req.mode.mode = LightModes.STATIC
-        req.mode.timeout = 3.0
+        req.mode.mode = LightModes.CIRCLE_COLORS
+        req.mode.timeout = 0.0
         req.mode.priority = 12
-        colors = [ColorRGBA(1.0,0.0,0.0,1.0), ColorRGBA(0.0,1.0,0.0,1.0), ColorRGBA(0.0,0.0,1.0,1.0)]
-        for color in colors:
-            for i in range(self._num_leds):
-                rospy.loginfo("Setting led " + str(i) + " to color (rgba):\n"+str(color))
-                req.mode.colors = [ColorRGBA(1.0,1.0,1.0,1.0)] * self._num_leds
-                req.mode.colors[i] = color
-                try:
-                    self._srv.call(req)
-                except Exception as e:
-                    rospy.logerr("%s",e)
-                    pass
-                rospy.sleep(req.mode.timeout)
+        req.mode.frequency = 1.0
+        req.mode.colors = [self._colors[self._idx%3][1]]
+        try:
+            self._srv.call(req)
+        except Exception as e:
+            rospy.logerr("%s",e)
+            pass
+        self._idx += 1
+        return
+
+    def stop(self):
+        self._timer.shutdown()
+        rospy.loginfo("stopping...")
+        req = SetLightModeRequest()
+        req.mode.mode = LightModes.NONE
+        req.mode.colors = [ColorRGBA()]
+        try:
+            self._srv.call(req)
+        except Exception as e:
+            pass
         return
         
 if __name__ == '__main__':
     rospy.init_node('test_light')
-    rospy.sleep(5)
+    rospy.sleep(2)
     test = TestLight()
-    while not rospy.is_shutdown():
-        test.execute()
+    test.start()
+    rospy.spin()
+    test.stop()
