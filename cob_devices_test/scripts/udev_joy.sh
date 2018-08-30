d7b92807ab3b43108fbc011f70e45a53cc81115c
fmessmer <felixmessmer@gmail.com>
2018-08-30 10:12:15 +0200

    improve udev script readability

--- cob_devices_test/scripts/udev_joy.sh
+++ cob_devices_test/scripts/udev_joy.sh
@@ -7,23 +7,18 @@ sudo rm -rf /dev/joypad
 JoyAttr1='ATTRS{idVendor}=="046d"'
 #JoyAttr2='ATTRS{idProduct}=="c21f"'
 
-sudo chmod 666 /dev/input/js0
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/input/js0) > /tmp/js0
-if grep -qs $JoyAttr1 /tmp/js0
-then
-    sudo ln -s input/js0 /dev/joypad
-fi
+for file in /dev/input/js*; do
+    #echo $file
+    sudo chmod 666 $file 2> /dev/null
+    sudo udevadm info -a -p $(udevadm info -q path -n $file) > /tmp/js${file: -1} 2> /dev/null
+    if grep -qs $JoyAttr1 /tmp/js${file: -1}
+    then
+        sudo ln -s input/js${file: -1} /dev/joypad
+    fi
+done
 
-sudo chmod 666 /dev/input/js1
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/input/js1) > /tmp/js1
-if grep -qs $JoyAttr1 /tmp/js1
-then
-    sudo ln -s input/js1 /dev/joypad
-fi
-
-sudo chmod 666 /dev/input/js2
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/input/js2) > /tmp/js2
-if grep -qs $JoyAttr1 /tmp/js2
-then
-    sudo ln -s input/js2 /dev/joypad
+if [ -L /dev/joypad ] && [ -e /dev/joypad ] ; then
+    echo "Joypad found"
+else
+    echo "Joypad not found"
 fi
--- cob_devices_test/scripts/udev_s300.sh
+++ cob_devices_test/scripts/udev_s300.sh
@@ -6,10 +6,11 @@ sudo rm -rf /dev/ttyScanLeft
 sudo rm -rf /dev/ttyScanRight
 
 for file in /dev/ttyUSB*; do
-  sudo chmod 666 $file
+  #echo $file
+  sudo chmod 666 $file 2> /dev/null
   sudo rm -f /tmp/usb${file: -1}
-  sudo udevadm info -a -p $(udevadm info -q path -n $file) > /tmp/usb${file: -1}
-  sudo chmod 666 /tmp/usb${file: -1}
+  sudo udevadm info -a -p $(udevadm info -q path -n $file) > /tmp/usb${file: -1} 2> /dev/null
+  sudo chmod 666 /tmp/usb${file: -1} 2> /dev/null
 done
 
 results=()
@@ -24,7 +25,7 @@ for file in /tmp/usb*; do
   fi
 done
 
-echo "found $count scanners: $results"
+echo -e "found $count scanners\n"
 
 if [[ ${results[0]} == ${results[1]} ]]; then
   ATTRSSerialFL=${results[0]}
@@ -40,9 +41,6 @@ fi
 ATTRSSerialFL="$( echo "$ATTRSSerialFL" | sed 's/ //g' )"
 ATTRSSerialR="$( echo "$ATTRSSerialR" | sed 's/ //g' )"
 
-echo $ATTRSSerialFL
-echo $ATTRSSerialR
-
 ## ScanFront ##
 ScanFrontAttr1='ATTRS{bInterfaceNumber}=="00"'
 ScanFrontAttr2=$ATTRSSerialFL
@@ -56,62 +54,36 @@ ScanRightAttr1='ATTRS{bInterfaceNumber}=="00"'
 ScanRightAttr2=$ATTRSSerialR
 
 
-sudo chmod 666 /dev/ttyUSB0
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/ttyUSB0) > /tmp/usb0
-if grep -qs $ScanFrontAttr1 /tmp/usb0  && grep -qs $ScanFrontAttr2 /tmp/usb0
-then
-    sudo ln -s ttyUSB0 /dev/ttyScanFront
+for file in /dev/ttyUSB*; do
+    sudo chmod 666 $file 2> /dev/null
+    sudo udevadm info -a -p $(udevadm info -q path -n $file) > /tmp/usb${file: -1} 2> /dev/null
+    if grep -qs $ScanFrontAttr1 /tmp/usb${file: -1}  && grep -qs $ScanFrontAttr2 /tmp/usb${file: -1}
+    then
+        sudo ln -s ttyUSB${file: -1} /dev/ttyScanFront
+    fi
+    if grep -qs $ScanLeftAttr1 /tmp/usb${file: -1}  && grep -qs $ScanLeftAttr2 /tmp/usb${file: -1}
+    then
+        sudo ln -s ttyUSB${file: -1} /dev/ttyScanLeft
+    fi
+    if grep -qs $ScanRightAttr1 /tmp/usb${file: -1}  && grep -qs $ScanRightAttr2 /tmp/usb${file: -1}
+    then
+        sudo ln -s ttyUSB${file: -1} /dev/ttyScanRight
+    fi
+done
+
+if [ -L /dev/ttyScanFront ] && [ -e /dev/ttyScanFront ] ; then
+    echo "ScanFront found"
+else
+    echo "ScanFront not found"
 fi
-if grep -qs $ScanLeftAttr1 /tmp/usb0  && grep -qs $ScanLeftAttr2 /tmp/usb0
-then
-    sudo ln -s ttyUSB0 /dev/ttyScanLeft
+if [ -L /dev/ttyScanLeft ] && [ -e /dev/ttyScanLeft ] ; then
+    echo "ScanLeft found"
+else
+    echo "ScanLeft not found"
 fi
-if grep -qs $ScanRightAttr1 /tmp/usb0  && grep -qs $ScanRightAttr2 /tmp/usb0
-then
-    sudo ln -s ttyUSB0 /dev/ttyScanRight
+if [ -L /dev/ttyScanRight ] && [ -e /dev/ttyScanRight ] ; then
+    echo "ScanRight found"
+else
+    echo "ScanRight not found"
 fi
 
-sudo chmod 666 /dev/ttyUSB1
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/ttyUSB1) > /tmp/usb1
-if grep -qs $ScanFrontAttr1 /tmp/usb1  && grep -qs $ScanFrontAttr2 /tmp/usb1
-then
-    sudo ln -s ttyUSB1 /dev/ttyScanFront
-fi
-if grep -qs $ScanLeftAttr1 /tmp/usb1  && grep -qs $ScanLeftAttr2 /tmp/usb1
-then
-    sudo ln -s ttyUSB1 /dev/ttyScanLeft
-fi
-if grep -qs $ScanRightAttr1 /tmp/usb1  && grep -qs $ScanRightAttr2 /tmp/usb1
-then
-    sudo ln -s ttyUSB1 /dev/ttyScanRight
-fi
-
-sudo chmod 666 /dev/ttyUSB2
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/ttyUSB2) > /tmp/usb2
-if grep -qs $ScanFrontAttr1 /tmp/usb2  && grep -qs $ScanFrontAttr2 /tmp/usb2
-then
-    sudo ln -s ttyUSB2 /dev/ttyScanFront
-fi
-if grep -qs $ScanLeftAttr1 /tmp/usb2  && grep -qs $ScanLeftAttr2 /tmp/usb2
-then
-    sudo ln -s ttyUSB2 /dev/ttyScanLeft
-fi
-if grep -qs $ScanRightAttr1 /tmp/usb2  && grep -qs $ScanRightAttr2 /tmp/usb2
-then
-    sudo ln -s ttyUSB2 /dev/ttyScanRight
-fi
-
-sudo chmod 666 /dev/ttyUSB3
-sudo udevadm info -a -p $(udevadm info -q path -n /dev/ttyUSB3) > /tmp/usb3
-if grep -qs $ScanFrontAttr1 /tmp/usb3 && grep -qs $ScanFrontAttr2 /tmp/usb3
-then
-    sudo ln -s ttyUSB3 /dev/ttyScanFront
-fi
-if grep -qs $ScanLeftAttr1 /tmp/usb3  && grep -qs $ScanLeftAttr2 /tmp/usb3
-then
-    sudo ln -s ttyUSB3 /dev/ttyScanLeft
-fi
-if grep -qs $ScanRightAttr1 /tmp/usb3  && grep -qs $ScanRightAttr2 /tmp/usb3
-then
-    sudo ln -s ttyUSB3 /dev/ttyScanRight
-fi
